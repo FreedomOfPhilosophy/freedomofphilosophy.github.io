f04a92da88860a5b444c3630f107ac9f634d4238
FreedomOfPhilosophy <freedomofphilosophy@gmail.com>
2024-08-08 05:26:45 +0200

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,12 @@
+// assets/script.js
 function toggleMode() {
     document.body.classList.toggle('night');
     document.body.classList.toggle('day');
-
+    
     const container = document.querySelector('.container');
     container.classList.toggle('night');
     container.classList.toggle('day');
-
+    
     const links = document.querySelectorAll('.link');
     links.forEach(link => {
         link.classList.toggle('night');
